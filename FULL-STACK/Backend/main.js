5f91b033b9db02ff8f60bf2b0bce403f7972e904
Coder-Xylem <abdullahsknitrr@gmail.com>
2025-05-14 17:06:11 +0530

    Update main.js

--- FULL-STACK/Backend/main.js
+++ FULL-STACK/Backend/main.js
@@ -31,7 +31,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5173/write',
   'http://localhost:5173/read',
-  'https://revision-amber.vercel.app/',
+  'https://revision-amber.vercel.app',
   'https://revision-amber.vercel.app/write',
   'https://revision-amber.vercel.app/read',
   'http://localhost:3000',
